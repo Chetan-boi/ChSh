1255c5193b3b730ef763f2cfb5b874f733a9771c
Chetan-boi <Chetan140209@gmail.com>
2026-07-04 20:54:16 +0530

    QOL updates

--- src/executor.cpp
+++ src/executor.cpp
@@ -1,16 +1,15 @@
 #include "executor.hpp"
-#include "lexer.hpp"
 #include <algorithm>
 #include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <iterator>
 #include <string>
-#include <filesystem>
-#include <sys/wait.h>
-#include <vector>
-#include <iostream>
-#include <unistd.h>
 #include <sys/time.h>
-#include <fstream>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
 
 using Command = std::vector<std::string>;
 using Output = std::string;
@@ -21,109 +20,117 @@ bool shouldSave = false;
 bool wasSuccessfull = false;
 
 namespace ShellCommands {
-  Output cd(const Directory dir,Directory& prevDir) {
-    try {
-      auto temp = std::filesystem::current_path();
-      std::filesystem::current_path(dir);
-      prevDir = temp;
-      return "Switched Dir";
-    }
-    catch(const std::exception& e) {
-      std::cout << e.what() << std::endl;
-      return e.what();
-    }
+Output cd(const Directory dir, Directory &prevDir) {
+  try {
+    auto temp = std::filesystem::current_path();
+    std::filesystem::current_path(dir);
+    prevDir = temp;
+    return "Switched Dir";
+  } catch (const std::exception &e) {
+    std::cout << e.what() << std::endl;
+    return e.what();
+  }
+}
+
+int runBinary(const Command &command) {
+  int fd[2];
+  pipe(fd);
+  pid_t pid = fork();
+  if (pid == -1) {
+    std::cout << "fork failed" << std::endl;
+    close(fd[1]);
+    close(fd[0]);
+    return 1;
   }
 
-  int runBinary(const Command& command) {
-    int fd[2];
-    pipe(fd);
-    pid_t pid = fork();
-    if (pid == -1) {
-      std::cout << "Command fork failed, returned -1" << std::endl;
-      return 1;
-    }
-    
-    if (pid > 0) {
-      int status {};
+  if (pid > 0) {
+    int status{};
 
-      if (shouldSave) {
-        close(fd[1]);
-        char buffer[4096];
-        ssize_t bytesRead;
+    if (shouldSave) {
+      close(fd[1]);
+      char buffer[4096];
+      ssize_t bytesRead;
 
-        while((bytesRead = read(fd[0], buffer, sizeof(buffer))) > 0) {
-          command_output.append(buffer,bytesRead);
-        }
-
-        close(fd[0]);
-        waitpid(pid, &status, 0);
-
-        if (WIFEXITED(status)) {
-          if (WEXITSTATUS(status)) return 1;
-          return 0;
-        }
-        return 1;
+      while ((bytesRead = read(fd[0], buffer, sizeof(buffer))) > 0) {
+        command_output.append(buffer, bytesRead);
       }
+
+      close(fd[0]);
       waitpid(pid, &status, 0);
 
       if (WIFEXITED(status)) {
-        if (WEXITSTATUS(status)) return 1;
+        if (WEXITSTATUS(status))
+          return 1;
         return 0;
       }
+      return 1;
     }
-   
-    if (pid == 0) {
-      std::vector<char*> args;
-  
-      for (const auto& arg : command) {
-        args.emplace_back(const_cast<char*>(arg.c_str()));
-      }
-      args.emplace_back(nullptr);
-      if (shouldSave) {
+    waitpid(pid, &status, 0);
+    close(fd[1]);
+    close(fd[0]);
 
-        close(fd[0]);
-        dup2(fd[1],STDOUT_FILENO);
+    if (WIFEXITED(status)) {
+      if (WEXITSTATUS(status))
+        return 1;
+      return 0;
+    }
+  }
 
-        close(fd[1]);
-        execvp(args[0],args.data());
+  if (pid == 0) {
+    std::vector<char *> args;
 
-        perror("execvp");
-        std::cout << "Command Failed" << std::endl;
-        exit(EXIT_FAILURE);
-     }
-      
-      execvp(args[0],args.data());
+    for (const auto &arg : command) {
+      args.emplace_back(const_cast<char *>(arg.c_str()));
+    }
+    args.emplace_back(nullptr);
+    if (shouldSave) {
+
+      close(fd[0]);
+      dup2(fd[1], STDOUT_FILENO);
+
+      close(fd[1]);
+      execvp(args[0], args.data());
 
       perror("execvp");
       std::cout << "Command Failed" << std::endl;
       exit(EXIT_FAILURE);
     }
-    return 1;
-  }
 
-  int writeToFile(std::string FileName) {
-    std::ofstream outputFile(FileName);
+    execvp(args[0], args.data());
 
-    if (!outputFile.is_open()) {
-      std::cout << "Error opening file" << std::endl;
-      return 1;
-    }
-    outputFile << command_output;
-    outputFile.close();
-    return 0;
+    perror("execvp");
+    std::cout << "Command Failed" << std::endl;
+    exit(EXIT_FAILURE);
   }
+  return 1;
 }
 
-int executeCommand(Command& command) {
+int writeToFile(std::string FileName) {
+  std::ofstream outputFile(FileName);
+
+  if (!outputFile.is_open()) {
+    std::cout << "Error opening file" << std::endl;
+    return 1;
+  }
+  outputFile << command_output;
+  outputFile.close();
+  return 0;
+}
+} // namespace ShellCommands
+
+int executeCommand(Command &command) {
   command_output = "";
   shouldSave = false;
-  
+
   if (command.empty()) {
     std::cout << "Please Enter a command" << std::endl;
     return 1;
   }
 
-  auto found = std::find_if(command.begin(), command.end(), [](const std::string& s) { return s == "<" || s == ">" || s == "|"; });
+  auto found =
+      std::find_if(command.begin(), command.end(), [](const std::string &s) {
+        return s == "<" || s == ">" || s == "|";
+      });
   std::string op;
   Command toRight;
 
@@ -131,18 +138,15 @@ int executeCommand(Command& command) {
     shouldSave = true;
   }
 
-  
   if (found != command.end()) {
-    int pos= std::distance(command.begin(),found);
+    int pos = std::distance(command.begin(), found);
     op = command[pos];
-    Command tempToRight(command.begin()+pos+1,command.end());
-    Command tempToLeft(command.begin(),command.begin()+pos);
+    Command tempToRight(command.begin() + pos + 1, command.end());
+    Command tempToLeft(command.begin(), command.begin() + pos);
     toRight = tempToRight;
     command = tempToLeft;
   }
 
-
-  
   auto commandType = command[0];
   static Directory prevDir;
 
@@ -152,16 +156,17 @@ int executeCommand(Command& command) {
         std::cout << "No Prev directory Found" << std::endl;
         return 1;
       }
-      std::cout << ShellCommands::cd(prevDir,prevDir) << std::endl;
+      std::cout << ShellCommands::cd(prevDir, prevDir) << std::endl;
       return 0;
     }
 
     if (command[1] == "~") {
-      std::cout << ShellCommands::cd("/Users/chetan",prevDir) << std::endl;
+      std::cout << ShellCommands::cd("/Users/chetan", prevDir) << std::endl;
       return 0;
     }
 
-    if (ShellCommands::cd(command[1],prevDir)!="Switched Dir") return 1;
+    if (ShellCommands::cd(command[1], prevDir) != "Switched Dir")
+      return 1;
     return 0;
   }
 
@@ -180,13 +185,14 @@ int executeCommand(Command& command) {
   }
 
   else {
-   wasSuccessfull =  ShellCommands::runBinary(command);
+    wasSuccessfull = ShellCommands::runBinary(command);
   }
 
   if (op == ">") {
-      return ShellCommands::writeToFile(toRight[0]);
+    return ShellCommands::writeToFile(toRight[0]);
   }
 
-  if (!wasSuccessfull) return 0;
+  if (!wasSuccessfull)
+    return 0;
   return 1;
 }
